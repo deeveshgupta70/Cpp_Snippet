c857723f1485c86e4df00fed8cb7603092d91478
Deevesh_Gupta <65775611+deeveshgupta70@users.noreply.github.com>
2021-12-16 19:58:19 +0530

    initial Commit

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+#define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) ((a > b) ? b : a)
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector<vector <T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(vector<vector<T>> v) {cerr << "\n"; for (auto i : v) { for( auto t : i){cerr<<t; cerr << " ";} cerr<<"\n";}}
+/*****************************************************************************************************************************/
+
+/*           												SIEVE OF ERATOSTHENES
+#define m 100001
+vector<bool> s(m , true);
+void sieve(){
+    
+    s[0]= false;
+    s[1] = false;
+    
+    for( int i = 2; i*i <= m; i++){
+        
+        if( s[i]){
+            
+            for( int j = i*i ; j < m; j += i){
+                
+                if( s[j]) s[j] = false;
+            }
+        }
+    }
+}
+
+*******************************************************************************************************************************/
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+#endif
+fastio();
+ 
+
+return 0;
+
+}
